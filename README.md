2c81c54d0cef21caeb8620dcd312acda7992d573
bharadwaj6 <1695154+bharadwaj6@users.noreply.github.com>
2023-05-01 03:07:49 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+https://github.com/firecracker-microvm/firecracker/blob/main/src/api_server/swagger/firecracker.yaml
+
+https://github.com/weaveworks/ignite
---
+++ run3.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+curl --unix-socket /tmp/firecracker.sock -i \
+    -X PUT "http://localhost/logger" \
+    -H  "accept: application/json" \
+    -H  "Content-Type: application/json" \
+    -d '{
+           "log_path": "/dev/null",
+           "level": "Error",
+           "show_level": true,
+           "show_log_origin": false
+    }'
